dfc91eddc53d47dc1906a9fa60b558ff6faedb88
Marc Longoria <marclongoria@outlook.com>
2018-05-26 21:05:37 -0500

    Commit project.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿var x = new Vue({
+    el: "#index-page",
+    data: {
+        appName: "The Store",
+        product: {
+            imageUrl: "/images/noimage.png",
+            brand: "Shawn's stuff",
+            category: "Paper Products",
+            name: "Magic Writing Paper",
+            price: 12.99
+        }
+    }
+});
